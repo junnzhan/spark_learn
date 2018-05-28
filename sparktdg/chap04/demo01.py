425e7a1c4355f3903cf154e26cefc91263595f9c
junnzhan <jun_zhan@outlook.com>
2018-05-28 17:55:59 +0800

    schema

--- sparktdg/chap04/demo01.py
+++ sparktdg/chap04/demo01.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from pyspark.sql.session import SparkSession
-
+from pyspark.sql.types import *
 spark = SparkSession.builder.master('local[*]').appName('demo01').getOrCreate()
 
-# retail_schema = StructType([StructField('InvoiceNo', StringType, True),
-#                             StructField('StockCode', StringType, True),
-#                             StructField('Description', StringType, True),
-#                             StructField('Quantity', IntegerType, True),
-#                             StructField('InvoiceDate', TimestampType, True),
-#                             StructField('UnitPrice', DoubleType, True),
-#                             StructField('CustomerID', DoubleType, True),
-#                             StructField('Country', StringType, True)])
+retail_schema = StructType([StructField('InvoiceNo', StringType(), True),
+                            StructField('StockCode', StringType(), True),
+                            StructField('Description', StringType(), True),
+                            StructField('Quantity', IntegerType(), True),
+                            StructField('InvoiceDate', TimestampType(), True),
+                            StructField('UnitPrice', DoubleType(), True),
+                            StructField('CustomerID', DoubleType(), True),
+                            StructField('Country', StringType(), True)])
 
-df = spark.read.option('header', 'true').option('inferSchema', 'true').csv(
+df = spark.read.option('header', 'true').option('inferSchema', 'false').schema(retail_schema).csv(
     '../data/retail-data/by-day/2010-12-01.csv')
 df.show()
 df.printSchema()
